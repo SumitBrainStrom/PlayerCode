7644a04883a1bdee0bb4015ffc7e0e3e97badc93
SumitBrainStrom <140607232+SumitBrainStrom@users.noreply.github.com>
2023-10-12 13:34:42 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-videojs.registerPlugin('password', function(pluginOptions) {
+videojs.registerPlugin('US-Store-Player', function(pluginOptions) {
   var myPlayer = this,
     passwordToMatch = '',
     myModal,
@@ -10,17 +10,19 @@ videojs.registerPlugin('password', function(pluginOptions) {
     // Get a ModalDialog object
     ModalDialog = videojs.getComponent('ModalDialog');
 
-  myPlayer.muted(true);
+  myPlayer.muted(false);
 
   // +++ Display ModalDialog +++
   // Read password from options
   passwordToMatch = pluginOptions.password;
+  siteLocal = pluginOptions.locale;
+  email = pluginOptions.email;
 
   //Create a div in which to place HTML content
   newElement.setAttribute("style", "display:flex;justify-content:center;align-items:center;");
 
   // Create content for ModalDialog
-  newElement.innerHTML = '<div class="wrap"><h2>Enter Store Password</h2><br><input class="theFormInput" type="password" id="passwordInputID"><br><input id="formButtonID" class="theForm" type="submit" value="View"></div>';
+  newElement.innerHTML = '<div class="wrap"><h2>Enter Store Details</h2><p>Enter Locale</p><select class="theFormInput" name="siteInputID" id="siteInputID"><option value="US">US</option><option value="UK">UK</option></select><p>Enter Email</p><input class="theFormInput" type="email" id="emailInputID"><p>Enter Password</p><input class="theFormInput" type="password" id="passwordInputID"><br><input id="formButtonID" class="theForm" type="submit" value="View"></div>';
 
   // Be sure user cannot close ModalDialog, set content
   modalOptions.uncloseable = true;
@@ -44,16 +46,38 @@ videojs.registerPlugin('password', function(pluginOptions) {
     }
   });
 
+  // Add event listener if user presses Enter key after entering password
+  siteInput = newElement.querySelector('#siteInputID');
+  myPlayer.on(siteInput, 'keydown', function(event) {
+    if (event.keyCode === 13) {
+      closeModal();
+    }
+  });
+
+  // Add event listener if user presses Enter key after entering password
+  emailInput = newElement.querySelector('#emailInputID');
+  myPlayer.on(emailInput, 'keydown', function(event) {
+    if (event.keyCode === 13) {
+      closeModal();
+    }
+  });
+
   // +++ Check entered password against saved password and act accordingly +++
   function closeModal() {
     var userInputPassword = document.getElementById('passwordInputID').value;
+    var userInputSite = document.getElementById('siteInputID').value;
+    var userInputEmail = document.getElementById('emailInputID').value;
     // If passwords match close ModalDialog and play video
-    if (userInputPassword == passwordToMatch) {
+    if (userInputPassword == passwordToMatch && userInputSite == siteLocal && userInputEmail == email) {
       myModal.close();
       myPlayer.play();
       // If passwords do not match display dialog indicating so
-    } else {
+    } else if (userInputPassword != passwordToMatch){
       window.alert('Sorry, password is incorrect. Try again.')
+    } else if (userInputSite != siteLocal){
+      window.alert('Sorry, Local is incorrect. Try again.')
+    } else if (userInputEmail != email){
+      window.alert('Sorry, Email is incorrect. Try again.')
     }
   }
-});
+})
